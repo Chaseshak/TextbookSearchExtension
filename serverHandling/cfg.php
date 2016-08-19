60075bccd6eee338e44dc8f52212af0e91c006d9
schachenman@wisc.edu <schachenman@wisc.edu>
2016-08-19 10:08:41 -0500

    Test to see if cfg is properly ignored

--- serverHandling/cfg.php
+++ serverHandling/cfg.php
@@ -10,3 +10,4 @@
 define('AWS_ACCESS_ID', 'AKIAIZASJG3BFOSWSPAQ');
 define('AWS_SECRET_KEY', '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');
 define('ASSOCIATE_TAG', 'textboo06-20');
+// THIS IS A TEST
